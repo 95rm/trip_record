d551cfb2add983811d12eee67a08d3c1aa17a35c
EC2 Default User <ec2-user@ip-172-31-6-142.ap-northeast-1.compute.internal>
2023-03-09 15:38:09 +0000

    [add]検索機能、タグ機能一部修正

--- app/controllers/public/trip_plans_controller.rb
+++ app/controllers/public/trip_plans_controller.rb
@@ -7,9 +7,9 @@ class Public::TripPlansController < ApplicationController
   def create
     @trip_plan = TripPlan.new(trip_plan_params)
     @trip_plan.user_id = current_user.id
-    @tag_list = params[:trip_plan][:name_tag].split(nil)
+    @tag_list = params[:trip_plan][:name_tag].split(nil)#paramsでデータを
     if @trip_plan.save
-      @trip_plan.save_tag(@tag_list)
+      @trip_plan.save_tag(@tag_list)#save_tagはTripPlanモデルに記述している
     else
       render :new
     end
@@ -22,7 +22,7 @@ class Public::TripPlansController < ApplicationController
     @total = 0
   end
 
-  def research
+  def research #ransackを使い検索機能実装
     @q = TripPlan.ransack(params[:q])
     @trip_plans = @q.result
   end
@@ -34,7 +34,7 @@ class Public::TripPlansController < ApplicationController
   def update
     @trip_plan = TripPlan.find(params[:id])#idで保存したいデータを呼び出す
     if @trip_plan.update(trip_plan_params)#trip_planのデータを保存する
-      @trip_plan.save_tags(params[:trip_plan][:name_tag])#タグのデータを保存する
+      @trip_plan.save_tag(params[:trip_plan][:name_tag])#タグのデータを保存する
       redirect_to trip_plan_path(@trip_plan.id)#showページに飛ぶ
     else
       render :edit
